53560be98e23ff26ef644902ba7f3ddd34ffac95
Marcin Pilch <marcinpilch4@gmail.com>
2025-05-05 19:06:23 +0200

    Print current software version

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,9 +1,11 @@
 #include <spdlog/spdlog.h>
 
+#include "version.h"
+
 import Transaction;
 
 int main() {
-  spdlog::info("Welcome to Finance Tracker!");
+  spdlog::info("Welcome to Finance Tracker version: {}", APP_VERSION);
   core::Transaction transaction(100.0);
   spdlog::info("Transaction amount: {}", transaction.getAmount());
   transaction.setAmount(200.0);
