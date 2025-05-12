536599068d81b4ec5293b0521cd17efc1ce31a54
Marcin Pilch <marcinpilch4@gmail.com>
2025-05-12 19:56:58 +0200

    Add TransactionBuilder

--- core/test/test_transactionmapper.cpp
+++ core/test/test_transactionmapper.cpp
@@ -1,10 +1,5 @@
 #include <gtest/gtest.h>
 
-import TransactionMapper;
-
 TEST(TransactionMapperTest, Construction) {
-  std::shared_ptr<TransactionMapper> mapper;
-  ASSERT_NO_THROW(mapper = std::make_shared<TransactionMapper>());
-  ASSERT_TRUE(mapper);
-  EXPECT_NO_THROW(mapper.reset());
+
 }
