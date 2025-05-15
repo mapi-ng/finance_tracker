bda05be63f323ecf613c384f308c69827ee8a898
Marcin Pilch <marcinpilch4@gmail.com>
2025-05-15 21:34:10 +0200

    gcovr exclude throw branches due to noise - not so useful branch coverage info

--- scripts/run-coverage.sh
+++ scripts/run-coverage.sh
@@ -4,6 +4,7 @@ gcovr -r . \
     --exclude-unreachable-branches \
     -e '.*/CMakeFiles.*' \
     -e '.*/test.*' \
+    --exclude-throw-branches \
     --print-summary \
     --sonarqube \
     --output coverage.xml
