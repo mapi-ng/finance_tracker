3eba7d1fa8783642e79a9d091ca4be70e391a792
mapi-ng <41466313+mapi-ng@users.noreply.github.com>
2025-06-03 19:44:57 +0200

    Add TransactionBuilder

--- core/src/Transaction.cpp
+++ core/src/Transaction.cpp
@@ -6,29 +6,63 @@ import Utils;
 
 namespace core {
 
+export class TransactionBuilder;
+
 export class Transaction {
  public:
-  using Timestamp = std::chrono::system_clock::time_point;
-  explicit Transaction(unsigned int amount,
-              Timestamp timestamp = Timestamp::clock::now())
-      : amount_{amount}, timestamp_{timestamp} {}
-
-  unsigned int getAmount() const { return amount_; }
+  using Date = std::chrono::year_month_day;
 
   uuids::uuid getId() const { return id_; }
-  Timestamp getTimestamp() const { return timestamp_; }
+  Date getDate() const { return date_; }
+  unsigned int getAmount() const { return amount_; }
   std::string getDescription() const { return description_; }
+  std::string getCategory() const { return category_; }
+
   void setDescription(std::string_view description) {
     description_ = description;
   }
 
-  void setAmount(unsigned int amount) { amount_ = amount; }
+ private:
+  Transaction(const Date& date, unsigned int amount)
+      : date_{date},
+        amount_{amount} {}
+
+  const uuids::uuid id_{utils::generateUuid()};
+  Date date_;
+  unsigned int amount_;
+  std::string description_;
+  std::string category_;
+
+  friend class TransactionBuilder;
+};
+
+class TransactionBuilder {
+ public:
+  TransactionBuilder(const Transaction::Date& date, unsigned int amount)
+      : transaction_{date, amount} {}
+  TransactionBuilder& timestamp(const Transaction::Date& date) {
+    transaction_.date_ = date;
+    return *this;
+  }
+
+  TransactionBuilder& amount(unsigned int value) {
+    transaction_.amount_ = value;
+    return *this;
+  }
+  TransactionBuilder& description(const std::string &value) {
+    transaction_.description_ = value;
+    return *this;
+  }
+
+  TransactionBuilder& category(const std::string &value) {
+    transaction_.category_ = value;
+    return *this;
+  }
+
+  Transaction build() { return std::move(transaction_); }
 
  private:
-  uuids::uuid id_{utils::generateUuid()};
-  unsigned int amount_;
-  Timestamp timestamp_;
-  std::string description_;
+  Transaction transaction_;
 };
 
 }  // namespace core
