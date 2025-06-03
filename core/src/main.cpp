9ffb40a99339ce82f83377e8a75073252630705b
mapi-ng <41466313+mapi-ng@users.noreply.github.com>
2025-06-03 19:42:53 +0200

    Create transaction module

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
 #include <spdlog/spdlog.h>
 
+import Transaction;
+
 int main() {
-    spdlog::info("Welcome to Finance Tracker!");
-    return 0;
+  spdlog::info("Welcome to Finance Tracker!");
+  core::Transaction transaction(100.0);
+  spdlog::info("Transaction amount: {}", transaction.getAmount());
+  transaction.setAmount(200.0);
+  spdlog::info("Updated transaction amount: {}", transaction.getAmount());
+  return 0;
 }
---
+++ core/test/test_transaction.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+import Transaction;
+
+class TransactionTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    // Initialize shared resources
+  }
+
+  void TearDown() override {
+    // Clean up resources if needed
+  }
+
+  core::Transaction transaction{100};
+};
+
+TEST_F(TransactionTest, ParameterizedConstructor) {
+  EXPECT_EQ(transaction.getAmount(), 100);
+}
+
+TEST_F(TransactionTest, SetAmount) {
+  transaction.setAmount(200);
+  EXPECT_EQ(transaction.getAmount(), 200);
+}
+
+TEST_F(TransactionTest, NegativeAmount) {
+  transaction.setAmount(-50);
+  EXPECT_EQ(transaction.getAmount(), -50);
+}
