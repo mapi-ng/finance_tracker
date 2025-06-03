c15d27e960205cedbad06c65a49fdd76d8c32185
mapi-ng <41466313+mapi-ng@users.noreply.github.com>
2025-06-03 19:44:27 +0200

    Generate sonar properties with current version

--- cmake/versionize.cmake
+++ cmake/versionize.cmake
@@ -3,6 +3,16 @@
 # Define path for generated file
 set(VERSION_HEADER "${CMAKE_BINARY_DIR}/generated/version.h")
 
+# Generate root of the version tag, without the 'v' prefix
+execute_process(
+    COMMAND git describe --tags --abbrev=0
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_VERSION_ROOT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+string(REGEX REPLACE "^v" "" GIT_VERSION_ROOT "${GIT_VERSION_ROOT}")
+
+# Generate the full version string
 execute_process(
     COMMAND git describe --tags --dirty --always
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -18,3 +28,17 @@ file(APPEND ${VERSION_HEADER} "constexpr auto APP_VERSION = \"${GIT_VERSION}\";\
 
 # Include the generated file directory
 include_directories(${CMAKE_BINARY_DIR}/generated)
+
+# Generate SonarQube properties file
+
+file(WRITE "${CMAKE_SOURCE_DIR}/sonar-project.properties"
+"sonar.projectName=${PROJECT_NAME}
+sonar.projectKey=mapi-ng_${PROJECT_NAME}
+sonar.organization=mapi-ng
+sonar.projectVersion=${GIT_VERSION_ROOT}
+
+sonar.sources=.
+sonar.sourceEncoding=UTF-8
+
+sonar.cfamily.enableModules=true
+")
